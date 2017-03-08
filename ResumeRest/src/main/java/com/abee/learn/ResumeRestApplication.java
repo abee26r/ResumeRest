79033d7789f018c73ef285b13fc6f41c225bb8b7
xx61691 <abilash.ramachandran@pnc.com>
2017-03-08 10:48:08 -0500

    init

---
+++ ResumeRest/src/main/java/com/abee/learn/ResumeRestApplication.java
@@ -0,0 +1,12 @@
+package com.abee.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeRestApplication.class, args);
+	}
+}
